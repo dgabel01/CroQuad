c8e9fe0b75ea17021089ab526024ae2ac8621e6a
Dominik Gabela <gabela.dominik@gmail.com>
2024-03-23 11:48:03 +0100

    Whatsapp test  ios mobile

--- components/HomePageComponents/WhatsappButton.tsx
+++ components/HomePageComponents/WhatsappButton.tsx
@@ -3,22 +3,34 @@ import React from 'react'
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
 import logo from '../../public/logoblack.jpg'
 
+
 const WhatsappButton = () => {
-return (
-    <>
-        <FloatingWhatsApp 
-             avatar={logo.src}
-            onSubmit={() => window.open('whatsapp://send?phone=+385953770460')}
-            accountName='Cro Quad'
-            chatboxHeight={350}
-            phoneNumber='+385 95 702 1991'
-            chatMessage='Hello there! 🤝 
-            How can we help?
-            Note for Apple users: please use the WhatsApp icon on the page bottom to message us, thanks!
-            '
-        />    
-    </>
-)
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream;
+
+    const openWhatsApp = () => {
+        const whatsappLink = 'whatsapp://send?phone= +385953770460';
+        if (isIOS) {
+            window.location.href = whatsappLink;
+        } else {
+            window.open(whatsappLink, '_blank');
+        }
+    };
+
+    return (
+        <>
+            <FloatingWhatsApp 
+                avatar={logo.src}
+                onSubmit={openWhatsApp}
+                accountName='Cro Quad'
+                chatboxHeight={400}
+                phoneNumber='+385953770460'
+                chatMessage={`Hello there! 🤝 
+                How can we help?
+                Note for Apple users: please use the WhatsApp icon on the page bottom to message us, thanks!
+                `}
+            />    
+        </>
+    );
 }
 
 export default WhatsappButton
