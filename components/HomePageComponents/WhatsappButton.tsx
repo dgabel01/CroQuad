ad55f74d123a8bd766a3320d7e1b094ac170c511
Dominik Gabela <gabela.dominik@gmail.com>
2024-03-23 11:50:39 +0100

    Deployment fix

--- components/HomePageComponents/WhatsappButton.tsx
+++ components/HomePageComponents/WhatsappButton.tsx
@@ -1,36 +1,40 @@
-"use client"
-import React from 'react'
-import { FloatingWhatsApp } from 'react-floating-whatsapp'
-import logo from '../../public/logoblack.jpg'
-
+import React from 'react';
+import { FloatingWhatsApp } from 'react-floating-whatsapp';
+import logo from '../../public/logoblack.jpg';
 
 const WhatsappButton = () => {
-    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream;
+    // Check if navigator is available (i.e., in the browser)
+    const isBrowser = typeof window !== 'undefined';
+    const isIOS = isBrowser && /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream;
 
     const openWhatsApp = () => {
-        const whatsappLink = 'whatsapp://send?phone= +385953770460';
-        if (isIOS) {
-            window.location.href = whatsappLink;
-        } else {
-            window.open(whatsappLink, '_blank');
+        const whatsappLink = 'whatsapp://send?phone=+385953770460';
+        if (isBrowser) {
+            if (isIOS) {
+                window.location.href = whatsappLink;
+            } else {
+                window.open(whatsappLink, '_blank');
+            }
         }
     };
 
     return (
         <>
-            <FloatingWhatsApp 
-                avatar={logo.src}
-                onSubmit={openWhatsApp}
-                accountName='Cro Quad'
-                chatboxHeight={400}
-                phoneNumber='+385953770460'
-                chatMessage={`Hello there! 🤝 
-                How can we help?
-                Note for Apple users: please use the WhatsApp icon on the page bottom to message us, thanks!
-                `}
-            />    
+            {isBrowser && (
+                <FloatingWhatsApp 
+                    avatar={logo.src}
+                    onSubmit={openWhatsApp}
+                    accountName='Cro Quad'
+                    chatboxHeight={400}
+                    phoneNumber='+385953770460'
+                    chatMessage={`Hello there! 🤝 
+                    How can we help?
+                    Note for Apple users: please use the WhatsApp icon on the page bottom to message us, thanks!
+                    `}
+                />
+            )}
         </>
     );
-}
+};
 
-export default WhatsappButton
+export default WhatsappButton;
