bf8502406185d66d2288583f7c4c9a2f118ef38c
Dominik Gabela <gabela.dominik@gmail.com>
2024-03-23 11:54:27 +0100

    Remove device checking for whatsapp button

--- components/HomePageComponents/WhatsappButton.tsx
+++ components/HomePageComponents/WhatsappButton.tsx
@@ -1,40 +1,24 @@
-import React from 'react';
-import { FloatingWhatsApp } from 'react-floating-whatsapp';
-import logo from '../../public/logoblack.jpg';
+"use client"
+import React from 'react'
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+import logo from '../../public/logoblack.jpg'
 
 const WhatsappButton = () => {
-    // Check if navigator is available (i.e., in the browser)
-    const isBrowser = typeof window !== 'undefined';
-    const isIOS = isBrowser && /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream;
+return (
+    <>
+        <FloatingWhatsApp 
+            avatar={logo.src}
+            onSubmit={() => window.open('whatsapp://send?phone=+385953770460', '_self')}
+            accountName='Cro Quad'
+            chatboxHeight={350}
+            phoneNumber='+385953770460'
+            chatMessage='Hello there! 🤝 
+            How can we help?
+            Note for Apple users: please use the WhatsApp icon on the page bottom to message us, thanks!
+            '
+        />    
+    </>
+)
+}
 
-    const openWhatsApp = () => {
-        const whatsappLink = 'whatsapp://send?phone=+385953770460';
-        if (isBrowser) {
-            if (isIOS) {
-                window.location.href = whatsappLink;
-            } else {
-                window.open(whatsappLink, '_blank');
-            }
-        }
-    };
-
-    return (
-        <>
-            {isBrowser && (
-                <FloatingWhatsApp 
-                    avatar={logo.src}
-                    onSubmit={openWhatsApp}
-                    accountName='Cro Quad'
-                    chatboxHeight={400}
-                    phoneNumber='+385953770460'
-                    chatMessage={`Hello there! 🤝 
-                    How can we help?
-                    Note for Apple users: please use the WhatsApp icon on the page bottom to message us, thanks!
-                    `}
-                />
-            )}
-        </>
-    );
-};
-
-export default WhatsappButton;
+export default WhatsappButton
