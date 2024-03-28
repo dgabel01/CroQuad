5f4107965fc1475abf37694cbef42d7b78ce90c3
Dominik Gabela <gabela.dominik@gmail.com>
2024-03-28 17:58:21 +0100

    Favicon for Google search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,18 @@ import Tours from '../../components/HomePageComponents/Tours'
 import Reviews from '../../components/HomePageComponents/Reviews';
 import FrequentlyAsked from '../../components/HomePageComponents/FrequentlyAsked';
 import Map from '../../components/HomePageComponents/Map';
+import { Helmet } from 'react-helmet-async'; 
+
 
 
 export default function Home (){ 
 
   return (
     <>
+     <Helmet>
+        <link rel="icon" href="/src/app/favicon.ico" /> 
+      </Helmet>
+      
     <Hero/>
     <Tours/>
     <Reviews/>
