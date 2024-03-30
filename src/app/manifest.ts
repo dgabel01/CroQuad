32380c77295f5e7b7fd2c55f533b6358c6a54177
Dominik Gabela <gabela.dominik@gmail.com>
2024-03-30 12:23:17 +0100

    Change favicon for Google

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,7 +8,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: '/',
     scope:'/',
     lang:'en',
-    categories: ['quad bikes', 'internet', 'renting', 'hvar', 'croquad', 'tours', 'rent','adventures', 'safari', 'lunch', 'explore', 'nature'],
+    categories: ['quad bikes', 'internet', 'renting', 'hvar', 'croquad', 'cro quad', 'tours', 'rent','adventures', 'safari', 'lunch', 'explore', 'nature'],
     display: 'standalone',
     background_color: '#fff',
     theme_color: '#fff',
