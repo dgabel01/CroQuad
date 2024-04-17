10f6b33ffefa3a86fcdc29b303fc53c901247ce5
Dominik Gabela <gabela.dominik@gmail.com>
2024-04-17 17:22:11 +0200

    Add analytics to document

--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
@@ -13,6 +14,15 @@ class MyDocument extends Document {
           <link rel="icon" type ="image/png" href="/public/android-chrome-192x192.png" />
           <link rel="manifest" href="/src/app/manifest.ts" />
           <meta name='description' content='CroQuad Hvar official website. Book the best quad ride on the island of Hvar.'/>
+
+          <Script strategy='lazyOnload' async src="https://www.googletagmanager.com/gtag/js?id=G-CREZFYP46K"></Script>
+          <Script id='ga-script' strategy='lazyOnload'>
+          {` window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-CREZFYP46K');`}
+          </Script>
         </Head>
         <body>
           <Main />
