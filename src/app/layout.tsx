808e2936d7cec6203d44b3228eab6e74a4471c9f
Dominik Gabela <gabela.dominik@gmail.com>
2024-04-17 13:17:00 +0200

    Google analytics fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ import { Analytics } from "@vercel/analytics/react"
 import WhatsappButton from "../../components/HomePageComponents/WhatsappButton";
 import UnderDevelopmentAlert from "../../components/HomePageComponents/UnderDevelopmentAlert";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
+
 
 
 const mont= Montserrat({ subsets: ['latin'] });
@@ -29,6 +31,20 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico"/>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-CREZFYP46K">
+        </Script>
+
+        <Script id="google-analytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+        </Script>
+
       </head>
       <body className={mont.className}>
         <UnderDevelopmentAlert/>
